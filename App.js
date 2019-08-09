cd45b86c76dc79a460d70d15af987cc35b81953f
Ruth Baker <ruthmoog@gmail.com>
2019-08-09 14:37:02 +0100

    research: trying out victory

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryLabel } from 'victory-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Fizzy_O!</Text>
-    </View>
-  );
-}
+const data = [
+  { date: '01/08/19', reps: 3, label: 3 },
+  { date: '02/08/19', reps: 5, label: 5 },
+  { date: '03/08/19', reps: 1, label: 1 },
+  { date: '04/08/19', reps: 2, label: 2 }
+];
 
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to Fizzy_O!</Text>
+        {/* <VictoryChart width={350} theme={VictoryTheme.material}>
+            <VictoryBar data={data} x="date" y="reps" label="DATEEEEEEE" />
+        </VictoryChart> */}
+        <VictoryChart 
+          // theme={VictoryTheme.material}
+          domainPadding={20}
+          >
+          <VictoryBar 
+            data={data}
+            style={{ data: { fill: "#FF00FF" }, labels: { fill: '#FFFFFF'} }}
+            labels={(d) => d.y}
+            labelComponent={<VictoryLabel dy={30}/>}
+            x="date" y="reps"/>
+          <VictoryAxis 
+            
+          />
+        </VictoryChart>
+
+
+
+      </View>
+    );
+}}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff"
+  }
 });
--- README.md
+++ README.md
@@ -18,9 +18,16 @@ In the command line at the root directory, run the command `npm run test`:
 $ npm run test
 ```
 
+### Linting
+
+```console
+prettier --print-width 80 --no-semi --single-quote --trailing-comma es5 --write src/**/*.js
+```
+
 ## Tech stack
 
 - [Enzyme](https://airbnb.io/enzyme/docs/guides/react-native.html) - a JavaScript Testing utility
 - [Jest](https://jestjs.io/docs/en/tutorial-react-native) - a JavaScript testing framework
+- [Prettier](https://prettier.io/) - a linter
 
 ## Team
