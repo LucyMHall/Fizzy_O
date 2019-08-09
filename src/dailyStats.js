7091cdcf64a9e0962b53bf421b21ba524ed12028
Student <kaczanowskamonika@gmail.com>
2019-08-09 17:28:21 +0100

    feat: activate the buttons

--- src/dailyStats.js
+++ src/dailyStats.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class DailyStatsScreen extends Component {
   render() {
     return(
     <View style={styles.container}>
       <Text>Daily Stats</Text>
+      <Button
+          title="Back to homepage"
+          onPress={() => this.props.navigation.navigate("UserMain")}
+        />
     </View>
   );
 }}
--- src/userMainScreen.js
+++ src/userMainScreen.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class UserMainScreen extends Component {
   render() {
     return(
     <View style={styles.container}>
       <Text>Hi, (username)</Text>
+      <Button
+       title="View your progress"
+       onPress={() => this.props.navigation.navigate("DailyStats")}
+     />
+     <Button
+       title="Record a session"
+       onPress={() => this.props.navigation.navigate("RecordSession")}
+     />
     </View>
   );
 }}
