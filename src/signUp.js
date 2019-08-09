4e10846bcd542ce2f97bd48af3c6a41438876bd9
Petra Pogacsas <petra.pogacsas@gmail.com>
2019-08-09 16:18:49 +0100

    style: border

--- src/signUp.js
+++ src/signUp.js
@@ -1,26 +1,70 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default class HomeScreen extends Component {
   render() {
     return(
     <View style={styles.container}>
-      <Text>Welcome to Fizzy_O!</Text>
+        <Text style={styles.heading}>Welcome to Fizzy_O!</Text>
       <Text>Sign up</Text>
+         <TextInput 
+                style={styles.inputs} 
+          placeholder='Username'
+        />
+         <TextInput 
+                style={styles.inputs} 
+          placeholder='Email'
+        />
+         <TextInput 
+                style={styles.inputs} 
+          placeholder='Password'
+        />
+         <TextInput 
+                style={styles.inputs} 
+          placeholder='Confirm password'
+        />
       <Button
-          title="Submit"
+          title="Sign Up"
           onPress={() => this.props.navigation.navigate("Login")}
         />
 
       <Text>Already have an account?</Text>
+        <Button
+          title="Log In"
+          onPress={() => this.props.navigation.navigate("Login")}
+        />
     </View>
   );
 }}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    },
+    paddingBottom: '45%'
+  },
+
+  heading: {
+    fontSize: 16,
+    flex: 1,
+    fontFamily: "Marker Felt"
+  },
+  inputs: {
+    flex: 1,
+    width: '80%',
+    padding: 10,
+    fontFamily: "Marker Felt",
+    borderRadius: 20,
+    borderWidth: 0.5
+  },
+  multiInput: {
+    flex: 2,
+    width: '90%',
+    paddingTop: 20
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16
+  }
   });
+
