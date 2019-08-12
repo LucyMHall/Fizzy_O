01ab4d3ada7256bf4025ba904d86904b8439e5c5
Ruth Baker <ruthmoog@gmail.com>
2019-08-12 12:23:05 +0100

    fix: render usermain pages
    Co-authored-by: Lucy <lucymhall1995@gmail.com>
    Co-authored-by: Ruth <ruthmoog@gmail.com>"

--- src/userMainScreen.js
+++ src/userMainScreen.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
-import {AsyncStorage} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, ImageBackground } from "react-native";
+import { AsyncStorage } from "react-native";
 
 export default class UserMainScreen extends Component {
-  static navigationOptions = { header: null }
+  static navigationOptions = { header: null };
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   _retrieveUsernameData = async () => {
     try {
-      const value = await AsyncStorage.getItem('username');
+      const value = await AsyncStorage.getItem("username");
       if (value !== null) {
         // We have data!!
-        this.setState({ username: value })
-        console.log(value)
+        this.setState({ username: value });
+        console.log(value);
       } else {
         console.log("That value does not exist");
       }
@@ -25,36 +25,32 @@ export default class UserMainScreen extends Component {
     }
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this._retrieveUsernameData();
   }
 
   render() {
     return(
-      <View>
-        <ImageBackground source={require('./assets/pictures/G3.jpg')} style={styles.container}>
-        <Text>Let's get Fizzy-cal</Text>
-        {this.state.username &&
-          <Text>Hello {this.state.username}</Text>
-        }
-        <Button
-         title="View your progress"
-         onPress={() => this.props.navigation.navigate("DailyStats")}
-       />
-       <Button
-         title="Record a session"
-         onPress={() => this.props.navigation.navigate("RecordSession")}
-       />
-       </ImageBackground>
-      </View>
-    )
+      <ImageBackground source={require("./assets/pictures/G3.jpg")} style={styles.container}>
+          <Text>Let's get Fizzy-cal</Text>
+            <Text>Hello {this.state.username}</Text>
+          <Button
+            title="View your progress"
+            onPress={() => this.props.navigation.navigate("DailyStats")}
+          />
+          <Button
+            title="Record a session"
+            onPress={() => this.props.navigation.navigate("RecordSession")}
+          />
+      </ImageBackground>
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
