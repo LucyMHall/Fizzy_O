c8e28191075ad6120b748ac2b8193b88d230b0e1
petraartep <petra.pogacsas@gmail.com>
2019-08-12 13:39:05 +0100

    feat: picker save state

--- src/recordSession.js
+++ src/recordSession.js
@@ -1,40 +1,53 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Picker, ImageBackground } from 'react-native';
+import { StyleSheet, Text, Button, View, TextInput, Picker, ImageBackground } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 export default class RecordSessionScreen extends Component {
   static navigationOptions = { header: null }
 
   constructor(props){
     super(props);
+
     this.state = {
-      exercise: ""
+      exercise: "",
+      reps: []
     }
   }
+  
   render(){
     return(
       <ImageBackground source={require('./assets/pictures/G3.jpg')} style={styles.container}>
+        
         <Text>Record your session</Text>
         <Picker
-        style={{height: 50, width: 400}}
-          selectedValue={this.state.exercise}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({exercise: itemValue})
+          style={{ height: 200, width: 400 }} itemStyle={{ height: 200 }}
+          selectedValue={(this.state && this.state.exercise) || "lowRow"} 
+          onValueChange={(exerciseValue) =>
+            this.setState({exercise: exerciseValue})
           }>
-          <Picker.Item label="Low row" value="exercise"/>
-          <Picker.Item label="Sit ups" value="exercise"/>
-          <Picker.Item label="Quadriceps Stretch" value="exercise"/>
-          <Picker.Item label="Hamstring Stretch" value="exercise"/>
-          <Picker.Item label="Kick backs" value="exercise"/>
-          <Picker.Item label="Bridging" value="exercise"/>
-          <Picker.Item label="Clam Shell" value="exercise"/>
-          <Picker.Item label="Lunges" value="exercise"/>
+          <Picker.Item label="Low row" value="lowRow"/>
+          <Picker.Item label="Sit ups" value="sitUps"/>
+          <Picker.Item label="Quadriceps Stretch" value="quadricepsStretch"/>
+          <Picker.Item label="Hamstring Stretch" value="hamstringStretch"/>
+          <Picker.Item label="Kick backs" value="kickBacks"/>
+          <Picker.Item label="Bridging" value="bridging"/>
+          <Picker.Item label="Clam Shell" value="clamShell"/>
+          <Picker.Item label="Lunges" value="lunges"/>
         </Picker>
 
         <Text>Enter reps</Text>
 
         <TextInput
+          style={styles.textBoxes}
          placeholder="0"
          />
+
+        <Button
+          style={{ padddingTop: 100 }}
+          title="Submit"
+          onPress={() => this.props.navigation.navigate("UserMain")}
+        />
+    
     </ImageBackground>
   )};
 };
@@ -53,13 +66,13 @@ const styles = StyleSheet.create({
    textBoxes: {
     margin: 5,
     backgroundColor:'white',
-    width:300,
+    width:100,
     height: 40,
     fontSize:15,
     borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
-
+    padding: 10,
 
    }
 });
