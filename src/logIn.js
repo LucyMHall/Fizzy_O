44b5e0fe3df96875ac2918406a95c666afdfeb24
Monika Kaczanowska <46656126+monikakaczan@users.noreply.github.com>
2019-08-12 10:53:57 +0100

    Update logIn.js

--- src/logIn.js
+++ src/logIn.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground } from 'react-native';
 
 export default class LoginScreen extends Component {
   render() {
     return(
-    <View style={styles.container}>
-      <Text>Log In</Text>
-    </View>
+      <ImageBackground source={require('./assets/pictures/G3.jpg')} style={styles.container}>
+      <Text style={styles.textBox}>Log In</Text>
+      <TextInput
+          placeholder = 'Email'
+        />
+        <TextInput
+          placeholder = 'Password'
+        />
+        <Button
+          title="Log in"
+          onPress={() => this.props.navigation.navigate("UserMain")}
+        />
+    </ImageBackground>
   );
 }}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textBox: {
+  fontFamily: 'Helvetica'
+  }
 });
